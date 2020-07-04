d6f6d100b2aa3e47edf41809dc6446154eb613bd
Walukagga Patrick <pwalukagga@gmail.com>
2020-07-04 10:17:22 +0300

    Implement collections transactions
    - Add Collections class objects for collection transaction objects.
    - Add collect method to the class.
    - Add retreive collection transaction methods.

---
+++ zengapay/collection.py
@@ -0,0 +1,47 @@
+from .client import ZengaPayAPI
+from .utils import validate_number, validate_phone_number, validate_string
+
+
+class Collections(ZengaPayAPI):
+
+    def collect(self, payload):
+        payload["msisdn"] = validate_phone_number(payload.get("msisdn"))
+        payload["amount"] = validate_number(payload.get("amount"))
+        payload["external_reference"] = validate_string(payload.get("external_reference"))
+        payload["narration"] = validate_string(payload.get("narration"))
+
+        url = f"{self.config.base_url}/collections"
+        res = self.request("POST", url, post_data=payload)
+
+        return res.json()
+
+    def get_all_collections(self, url=None):
+        if url is None:
+            url = f"{self.config.base_url}/collections"
+        res = self.request("GET", url)
+
+        return res.json()
+    
+    def get_collection(self, transaction_ref):
+        url = f"{self.config.base_url}/collections/{validate_string(transaction_ref)}"
+        res = self.request("GET", url)
+
+        return res.json()
+    
+    def get_transaction_status(self, transaction_ref):
+        url = f"{self.config.base_url}/collections/{validate_string(transaction_ref)}"
+        res = self.request("GET", url)
+
+        return res.json()["data"]["transactionStatus"]
+    
+
+
+if __name__ == "__main__":
+    config = {
+        "ENVIRONMENT": os.environ.get("APP_SETTINGS", "sandbox"),
+        "BASE_URL": os.environ.get("BASE_URL", "https://api.sandbox.zengapay.com/v1"),
+        "USER_API_TOKEN": os.environ.get("API_TOKEN")
+    }
+
+    collection = Collections(config)
+    collection.get_all_collections()
